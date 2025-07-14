fb1ddf2be3cbfabe7538664f88b2857f976606d4
Joyson1819 <1912402@nec.edu.in>
2025-07-14 21:26:06 +0530

    Feat:props

---
+++ day-4/src/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Details = (props) => {
+  return (
+    <div>
+      <h1>This is my name :{props.name} Guyz</h1>
+      <h6>This is my age :{props.age} Guyz</h6>
+    </div>
+  )
+}
+
+export default Details
